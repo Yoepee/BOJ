5db32dad55313a18b4524235594dd8189a145405
Dongyeop <kdy2975@naver.com>
2025-05-17 04:35:53 +0900

    BOJ-EX: 5/17/2025, 4:35:52 AM

--- 1916번 - 최소비용 구하기/최소비용 구하기.js
+++ 1916번 - 최소비용 구하기/최소비용 구하기.js
@@ -9,32 +9,23 @@
 /*   Solved: 2025/05/17 04:20:07 by kdy2975       ###          ###   ##.kr    */
 /*                                                                            */
 /* ************************************************************************** */
-// const fs = require("fs");
-// const input = fs
-//   .readFileSync("input.txt", "utf8")
-//   .toString()
-//   .trim()
-//   .split("\n");
-
-// const n = Number(input[0]);
-// const m = Number(input[1]);
-// const busInfo = input.slice(2).map((info) => info.trim().split(" "));
 const fs = require("fs");
+const path = require("path");
+
 const input =
   process.platform === "linux"
-    ? fs.readFileSync("/dev/stdin", "utf8") // 백준 제출용
-    : fs.readFileSync("input.txt", "utf8"); // 로컬 테스트용
+    ? fs.readFileSync("/dev/stdin", "utf8")
+    : fs
+        .readFileSync(path.join(__dirname, "input.txt"), "utf8")
+        .toString()
+        .trim()
+        .split("\n");
 
-const lines = input.trim().split("\n");
+// 문제별 로직 작성
+const n = Number(input[0]);
+const m = Number(input[1]);
+const busInfo = input
+  .slice(2)
+  .map((info) => info.trim().split(" ").map(Number));
 
-// 👇 아래에서 문제에 맞게 입력 파싱 & 로직 작성
-function main() {
-  const n = Number(lines[0]);
-  const arr = lines[1].split(" ").map(Number);
-
-  // 예시: 배열 정렬 후 출력
-  arr.sort((a, b) => a - b);
-  console.log(arr.join(" "));
-}
-
-main();
+console.log(n, m, busInfo);
