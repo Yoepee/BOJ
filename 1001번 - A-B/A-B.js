8e1dd292655610bdf04f457215bb3929a07edd47
Dongyeop <kdy2975@naver.com>
2025-05-17 19:25:02 +0900

    BOJ-EX: 5/17/2025, 7:25:01 PM

---
+++ 1001번 - A-B/A-B.js
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                      :::    :::    :::     */
+/*   Problem Number: 1001                              :+:    :+:      :+:    */
+/*                                                    +:+    +:+        +:+   */
+/*   By: kdy2975 <boj.kr/u/kdy2975>                  +#+    +#+          +#+  */
+/*                                                  +#+      +#+        +#+   */
+/*   https://boj.kr/1001                           #+#        #+#      #+#    */
+/*   Solved: 2025/05/17 19:23:58 by kdy2975       ###          ###   ##.kr    */
+/*                                                                            */
+/* ************************************************************************** */
+
+const fs = require("fs");
+const path = require("path");
+
+const input =
+  process.platform === "linux"
+    ? fs.readFileSync("/dev/stdin", "utf8")
+    : fs
+        .readFileSync(path.join(__dirname, "input.txt"), "utf8")
+        .toString()
+        .trim()
+        .split(" ");
+
+const a = Number(input[0]);
+const b = Number(input[1]);
+
+console.log(a - b);
